bb133fe5d0175d8a5b9c5e1d8bacec0365faa549
Ayaz Uddin <email": null>
2016-04-20 19:43:38 -0400

    update answer migration and model

--- db/migrate/20160420145846_create_answers.rb
+++ db/migrate/20160420145846_create_answers.rb
@@ -2,6 +2,8 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :content, null: false
+      t.references :user, null: false
+      t.references :question, null: false
 
       t.timestamps null: false
     end
