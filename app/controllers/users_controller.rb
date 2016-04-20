654a3bfd9fc6068e862d8a64c2689ccee0d87608
Ayaz Uddin <email": null>
2016-04-20 13:23:28 -0400

    add routes for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+get '/users' do
+  @users = User.all
+  erb :'users/index'
+end
+
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    redirect '/users'
+  else
+    erb :'users/new'
+  end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'users/show'
+end
+
+get '/users/:id/edit' do
+  @user = User.find(params[:id])
+  erb :'users/edit'
+end
+
+put '/users/:id' do
+  @user = User.find(params[:id])
+  @user.assign_attributes(params[:user])
+  if @user.save
+    redirect '/users'
+  else
+    erb :'users/edit'
+  end
+end
+
+delete '/users/:id' do
+  @user = User.find(params[:id])
+  @user.destroy
+  redirect '/users'
+end
