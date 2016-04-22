b1a5cd916a264f32c025ab4a66abebc31a1a174e
Josh Wu <email": null>
2016-04-22 16:02:30 -0400

    revert question/show to original, working ajax show page is in answers.js

--- public/js/answers.js
+++ public/js/answers.js
@@ -25,3 +25,65 @@ $(document).ready(function(){
   });
 
 });
+
+
+//question show page
+<div class="container">
+  <div class="main-content">
+    <div class="questions-content">
+
+      <div class="votes">
+        <a href="#">Up</a>
+          <p class="score"><%=@question.points || 0 %></p>
+        <a href="#">Down</a><br>
+      </div>
+
+      <div class="questions">
+        <h1><%=@question.title%></h1>
+        <p><%=@question.content%></p>
+        <% if session[:user_id].to_i == @question.user_id %>
+          <a href='/questions/<%=@question.id%>/edit'>Edit Post</a>
+        <% end %>
+
+      </div>
+
+    </div>
+
+    <div class="questions-comments">
+    <% if @question.comments %>
+      <% @question.comments.each do |comment|%>
+        <div class="questions-comments">
+          <%= comment.content %>
+        <% if comment.user_id == session[:user_id].to_i %>
+          <a href="/comments/<%=comment.id%>/edit">Edit Comment</a>
+        <% end %>
+        </div>
+      <% end %>
+    <% end %>
+      <br>
+      <a href="/questions/<%=@question.id%>/comments/new">Add a comment to post</a><br>
+    </div>
+
+
+  <div class="main-content">
+     <% if @question.answers %>
+    <div class="answer-content">
+
+      <div class="votes">
+        <a href="#">Up</a>
+          <p class="score">#</p>
+        <a href="#">Down</a><br>
+      </div>
+
+      <% @question.answers.each do |answer| %>
+       <%= erb :'/answers/_answer.html', locals: {answer: answer}%>
+
+      <% end %>
+      <% end %>
+
+    </div>
+      <button id="answer_button" class="button center">Answer</button>
+ <%= erb :'/answers/_new_form.html' %>
+  </div>
+</div>
+
